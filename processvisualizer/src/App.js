29ec30d2c3adffe4a30c5e3bcccabdc29d1e4f08
TheFireChicken <civenes99@gmail.com>
2023-11-03 10:54:25 +0100

    First Push

---
+++ processvisualizer/src/App.js
@@ -0,0 +1,15 @@
+import './css/processmap.css';
+
+function App() {
+  return (
+    <div class="parent">
+     <div class="headnotes">1 </div> 
+      <div class="views">2 </div>
+      <div class="processes">3 </div>
+      <div class="meta"> 4</div>
+      <div class="processmap">5 </div>
+    </div>
+  );
+}
+
+export default App;
