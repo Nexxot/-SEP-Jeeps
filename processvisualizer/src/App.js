33f2e9966a674948c4b03f531918e9e7d2411802
Nils <nils.eilinghoff@t-online.de>
2023-11-03 12:00:31 +0100

    Started NavBar

--- processvisualizer/src/App.js
+++ processvisualizer/src/App.js
@@ -1,9 +1,31 @@
 import './css/processmap.css';
+import './css/navbar.css'
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
   return (
     <div class="parent">
-     <div class="headnotes">1 </div> 
+      <div class="headnotes">
+          <Nav
+              activeKey="/home"
+              onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+          >
+              <Nav.Item>
+                  <Nav.Link eventKey="Add">ADD</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                  <Nav.Link eventKey="Delete">DELETE</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                  <Nav.Link eventKey="link-2">Link</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                  <Nav.Link eventKey="disabled" disabled>
+                      Disabled
+                  </Nav.Link>
+              </Nav.Item>
+          </Nav>
+      </div>
       <div class="views">2 </div>
       <div class="processes">3 </div>
       <div class="meta"> 4</div>
