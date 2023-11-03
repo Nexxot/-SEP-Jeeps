b38117f28774f789f46f3e0d985e088c51bfdc54
TheFireChicken <civenes99@gmail.com>
2023-11-03 17:26:09 +0100

    Center Attempt

--- processvisualizer/src/procesmap/ProcessMap.js
+++ processvisualizer/src/procesmap/ProcessMap.js
@@ -1,32 +1,57 @@
-import React, { useState } from 'react'
-import ProcessMapProcess from './ProcessMapNodes'
-import ReactFlow, {
-  MiniMap,
-  Controls,
-  Background,
-  useNodesState,
-  useEdgesState,
-  addEdge,
-} from 'reactflow';
+import React,{useRef, useEffect, useState} from 'react';
+import ReactFlow from 'reactflow';
 import 'reactflow/dist/style.css';
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
 export default function ProcessMap() {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  let [nodes, setNodes] = useState([]);
 
+  let edges = [
+    { id: 'edge1', source: 'node1', target: 'node2', animated: false },
+    { id: 'edge2', source: 'node2', target: 'node3', animated: false },
+  ];
 
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    if (divRef.current) {
+      // Calculate the center of the div and set it in the center constant
+      //center.current = getCenterOfProcessMap(divRef.current);
+      nodes = setNodes([{
+        id: 'node1',
+        type: 'default',
+        data: { label: 'Node 1' },
+        position: { x: getCenterOfProcessMap().x, y: getCenterOfProcessMap().y },
+      },
+      {
+        id: 'node2',
+        type: 'default',
+        data: { label: 'Node 2' },
+        position: { x: getCenterOfProcessMap().x, y: getCenterOfProcessMap().y + 100},
+      },
+      {
+        id: 'node3',
+        type: 'default',
+        data: { label: 'Node 3' },
+        position: { x: getCenterOfProcessMap().x, y: getCenterOfProcessMap().y + 200 },
+      }]);
+    }
+  }, []); 
+
+  function getCenterOfProcessMap() {
+    // Get the dimensions of the div
+    //const divRect = mapRef.current.getBoundingClientRect();
+  
+    // Calculate the center coordinates
+    let divRect = divRef.current.getBoundingClientRect();
+    const centerX = divRect.left + divRect.left * 2;
+    const centerY = divRect.top + divRect.top * 2;
+  
+    return { x: centerX, y: centerY };
+  }
 
   return (
-    <div></div>
-    // <ReactFlow
-    //   nodes={nodes}
-    //   edges={edges}
-    //   fitView
-    // />
+    <div ref={divRef} style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow nodes={nodes} edges={edges} />
+    </div>
   )
 }
--- processvisualizer/src/procesmap/ProcessMapNodes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ProcessMapProcess(nodes) {
-  return (
-    <div>
-      {nodes.length}
-    </div>
-  )
-}
